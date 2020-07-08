298be3bd9d152006f20815387a37abe3bdbcee4a
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-08 14:13:18 +0200

    Code cleanup

--- lambdas/request-klima-stock-update/request-klima-stock-update.js
+++ lambdas/request-klima-stock-update/request-klima-stock-update.js
@@ -3,10 +3,7 @@ const getStocks = require('./helpers/get-stocks.js')
 module.exports.handler = async (event) => {
   let statusCode = 200
   const headers = { 'Access-Control-Allow-Origin': '*' }
-  var res = {
-    statusCode,
-    headers
-  }
+  var res = { statusCode, headers }
   return await getStocks()
     .then(stocks => {
       res.body = JSON.stringify(stocks)
@@ -15,6 +12,6 @@ module.exports.handler = async (event) => {
     .catch(err => {
       console.log(err)
       res.statusCode = 500
-      res.body = JSON.stringify({ message: 'An error occured while fetching updated stocks...' })
+      res.body = JSON.stringify({ message: 'An error occured while fetching Collmex stocks data...' })
     })
 }
