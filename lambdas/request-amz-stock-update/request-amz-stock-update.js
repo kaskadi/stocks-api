75601b699f65444044e1981196b27ae635ed19f8
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-08 14:23:17 +0200

    Moved require ahead of export

--- lambdas/request-amz-stock-update/request-amz-stock-update.js
+++ lambdas/request-amz-stock-update/request-amz-stock-update.js
@@ -1,3 +1,6 @@
+const getCountryCodes = require('./helpers/get-country-codes.js')
+const requestReports = require('./helpers/request-reports.js')
+
 module.exports.handler = async (event) => {
   // care with throttling. For now there is none because our SellerID and tokens only work for EU which has a single endpoint. But this may be different in the future!
   var res = { 
@@ -6,8 +9,6 @@ module.exports.handler = async (event) => {
       'Access-Control-Allow-Origin': '*'
     }
   }
-  const getCountryCodes = require('./helpers/get-country-codes.js')
-  const requestReports = require('./helpers/request-reports.js')
   const countryCodes = getCountryCodes(event.queryStringParameters)
   if (countryCodes.length === 0) {
     res.statusCode = 400
