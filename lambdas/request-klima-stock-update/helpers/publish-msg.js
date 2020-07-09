60b540b287f11e23f328e85b12cc2aac20687373
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-09 12:24:30 +0200

    Updated environment variables

--- lambdas/request-klima-stock-update/helpers/publish-msg.js
+++ lambdas/request-klima-stock-update/helpers/publish-msg.js
@@ -3,7 +3,7 @@ const SQS = new AWS.SQS({ apiVersion: '2012-11-05' })
 
 module.exports = (stocks) => {
   const params = {
-    QueueUrl: 'https://sqs.eu-central-1.amazonaws.com/374163881813/klima-stocks-update-queue',
+    QueueUrl: process.env.QUEUE_URL,
     MessageBody: JSON.stringify(stocks)
   }
   return SQS.sendMessage(params).promise()
